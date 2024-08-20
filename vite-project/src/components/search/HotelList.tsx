d4b3c49ff8c10b09c61bfa264678121afd834768
janajovanovic1 <janamajajovanovic@gmail.com>
2024-08-20 07:27:38 +0200

    Tripadvisor - Pagination for Hotels & Restaurants

--- vite-project/src/components/search/HotelList.tsx
+++ vite-project/src/components/search/HotelList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useLoading } from '../../hooks/useLoading';
 import { useSearchedHotels } from '../../hooks/useSearchedHotels';
 import { useSearchTerm } from '../../hooks/useSearchTerm.hook';
@@ -5,6 +6,8 @@ import Loader from '../Loader';
 import HotelCard from './HotelCard';
 
 const HotelList = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   const { searchedHotels } = useSearchedHotels();
   const { searchTerm } = useSearchTerm();
   const { loading } = useLoading();
@@ -16,6 +19,13 @@ const HotelList = () => {
       </div>
     );
   }
+
+  useEffect(() => {
+    if (searchedHotels?.length > 0) {
+      setTotalPages(Math.ceil(searchedHotels.length / 12));
+    }
+    setCurrentPage(1);
+  }, [searchedHotels]);
 
   return (
     <>
@@ -35,10 +45,28 @@ const HotelList = () => {
             </div>
           )}
       <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 mt-5 mb-5'>
-        {searchedHotels?.map((hotel, idx) => (
-          <HotelCard key={idx} hotel={hotel} />
-        ))}
+      {searchedHotels
+          ?.slice(currentPage * 12 - 12, currentPage * 12)
+          .map((hotel, idx) => (
+            <HotelCard key={idx} hotel={hotel} />
+          ))}
       </div>
+
+      {searchedHotels && (
+        <div className='flex items-center justify-center gap-5 my-5'>
+          {[...Array(totalPages)].map((e, idx) => (
+            <div
+              className={`bg-green-400 px-3 rounded-full text-xl py-1 font-bold text-white cursor-pointer hover:bg-green-700 ${
+                currentPage === idx + 1 && ' activepagination'
+              }`}
+              key={e + ' ' + idx}
+              onClick={() => setCurrentPage(idx + 1)}
+            >
+              {idx + 1}
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 };
--- vite-project/src/components/search/RestaurantList.tsx
+++ vite-project/src/components/search/RestaurantList.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import { useLoading } from '../../hooks/useLoading';
 import { useSearchedRestaurants } from '../../hooks/useSearchedRestaurants';
 import { useSearchTerm } from '../../hooks/useSearchTerm.hook';
@@ -5,6 +7,8 @@ import Loader from '../Loader';
 import RestaurantCard from './RestaurantCard';
 
 const RestaurantList = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   const { searchedRestaurants } = useSearchedRestaurants();
   const { searchTerm } = useSearchTerm();
   const { loading } = useLoading();
@@ -16,6 +20,12 @@ const RestaurantList = () => {
       </div>
     );
   }
+  useEffect(() => {
+    if (searchedRestaurants?.length > 0) {
+      setTotalPages(Math.ceil(searchedRestaurants.length / 12));
+    }
+    setCurrentPage(1);
+  }, [searchedRestaurants]);
 
   return (
     <>
@@ -35,12 +45,30 @@ const RestaurantList = () => {
             </div>
           )}
       <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 mt-5 mb-5'>
-        {searchedRestaurants?.map((restaurant, idx) => (
-          <RestaurantCard key={idx} restaurant={restaurant} />
-        ))}
+          {searchedRestaurants
+          ?.slice(currentPage * 12 - 12, currentPage * 12)
+          .map((restaurant, idx) => (
+            <RestaurantCard key={idx} restaurant={restaurant} />
+          ))}
+  </div>
+
+{searchedRestaurants && (
+  <div className='flex items-center justify-center gap-5 my-5'>
+    {[...Array(totalPages)].map((e, idx) => (
+      <div
+        className={`bg-green-400 px-3 rounded-full text-xl py-1 font-bold text-white cursor-pointer hover:bg-green-700 ${
+          currentPage === idx + 1 && ' activepagination'
+        }`}
+        key={e + ' ' + idx}
+        onClick={() => setCurrentPage(idx + 1)}
+      >
+        {idx + 1}
       </div>
-    </>
-  );
+    ))}
+  </div>
+)}
+</>
+);
 };
 
 export default RestaurantList;
