4c53f9c134ad80f1900b882ff858858955ced6e6
KsenijaLazicc <lazicksenija2@gmail.com>
2024-08-24 09:10:39 +0200

    Tripadvisor - Responsiveness for Navbar & Menubar

--- vite-project/src/components/Navbar.tsx
+++ vite-project/src/components/Navbar.tsx
@@ -15,12 +15,12 @@ const Navbar = () => {
   }, [loggedIn]);
 
   return (
-    <div className='flex py-4 pl-20 justify-between navbar'>
+    <div className='flex py-4 sm:pl-20 sm:justify-between items-center navbar flex-col sm:flex-row'>
       <Link to={'/'} className='flex items-center gap-2'>
         <img src={logo} className='w-10 h-10 bg-green-400 rounded-full' />
         <h1 className='font-bold text-2xl'>Tripadvisor</h1>
       </Link>
-      <div className='flex justify-end gap-10 mr-20'>
+      <div className='flex justify-end gap-10 sm:mr-20'>
       {user && (
           <h1 className='font-semibold mt-2'>
             Welcome, <span className='text-green-500'>{user}</span> !
