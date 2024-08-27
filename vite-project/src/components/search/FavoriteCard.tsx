82fcc0dd408a6d9df5691b286003c1bd53927dce
janajovanovic1 <janamajajovanovic@gmail.com>
2024-08-27 12:59:30 +0200

    U poslednjoj verziji koda, komentar je uklonjen iz datoteke FavoriteCard.tsx na Tripadvisor projektu. Ovo može značiti da je prethodni komentar bio nepotreban ili da je njegovo objašnjenje sada obuhvaćeno drugim delom koda.

--- vite-project/src/components/search/FavoriteCard.tsx
+++ vite-project/src/components/search/FavoriteCard.tsx
@@ -44,18 +44,6 @@ const FavoriteCard = ({ favorite }: FavoriteCardProps) => {
               <span>{favorite.priceRange || '$'}</span>
             </div>
           </span>
-          {/* <span
-        onClick={handleFavorite}
-        className='inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2 cursor-pointer'
-      >
-        <div className='flex items-center'>
-          {favorites.includes(possibleFavorite) ? (
-            <FaHeart />
-          ) : (
-            <FaRegHeart />
-          )}
-        </div>
-      </span> */}
         </div>
       </div>
     </div>
